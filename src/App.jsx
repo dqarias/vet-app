4727f4ef474fa067c3c9d16a6b7f5205b17a7821
dqarias <dino.quispea@gmail.com>
2023-01-30 23:08:35 -0500

    Create react app using vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Form from "./components/Form";
+import Header from "./components/Header";
+import PatientList from "./components/PatientList";
+
+function App() {
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+      <Form />
+      <PatientList />
+    </div>
+  );
+}
+
+export default App;
